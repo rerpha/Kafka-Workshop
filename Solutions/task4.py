ecaddfa279a9041b64b6e07beaaf7a0c2cb239b7
Jack Harper <jack.harper@stfc.ac.uk>
2018-11-20 09:24:31 +0000

    fixing task 4

--- Solutions/task4.py
+++ Solutions/task4.py
@@ -1,15 +1,16 @@
 from itertools import islice
 from math import ceil
 from pykafka import KafkaClient
+from pykafka.common import OffsetType
 
 
 client = KafkaClient(hosts="hinata.isis.cclrc.ac.uk:9092")
 
 topic = client.topics[b'workshop_task4']
 
-consumer = topic.get_simple_consumer(consumer_group=b"mygroup")
+consumer = topic.get_simple_consumer(auto_offset_reset=OffsetType.LATEST, reset_offset_on_start=True)
 
-LAST_N_MESSAGES = 51
+LAST_N_MESSAGES = 3
 # how many messages should we get from the end of each partition?
 MAX_PARTITION_REWIND = int(ceil(LAST_N_MESSAGES / len(consumer._partitions)))
 # find the beginning of the range we care about for each partition
